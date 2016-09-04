7a326b4b6c2f5d178d9d4e400a3cfd6283f6a6be
Stéphane Dion <divers@sdion.net>
2016-09-04 08:45:08 +0200

    -

--- src/Readerself/CoreBundle/Controller/SettingController.php
+++ src/Readerself/CoreBundle/Controller/SettingController.php
@@ -6,14 +6,10 @@ use Readerself\CoreBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-use Minishlink\WebPush\WebPush;
-
-class DefaultController extends AbstractController
+class SettingController extends AbstractController
 {
     public function indexAction(Request $request)
     {
-        $this->get('readerself_core_manager_collection')->start();
-
-        return $this->render('AxipiFeedBundle::default.html.twig', []);
+        return $this->render('ReaderselfCoreBundle:Setting:index.html.twig', []);
     }
 }
