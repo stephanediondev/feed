669abd941d589395f4cb6682b4b15a0883a59824
Stéphane Dion <divers@sdion.net>
2016-10-05 16:28:28 +0200

    -

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
-git clone https://github.com/readerself/readerself-symfony.git
-cd readerself-symfony
+git init
+git remote add origin https://github.com/readerself/readerself-symfony.git
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
+php composer-setup.php --filename=composer
 php -r "unlink('composer-setup.php');"
 
-SYMFONY_ENV=prod ./composer.phar install --no-dev -o --prefer-dist
+SYMFONY_ENV=prod ./composer install --no-dev -o --prefer-dist
 bin/console doctrine:fixtures:load --append --fixtures="src/Readerself/CoreBundle/DataFixtures"
 cd web/client
+npm install -g bower
 bower install
 cd ../../
