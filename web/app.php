2490001e640740d3dd804d4c8a03e357337dccab
Stéphane Dion <divers@sdion.net>
2016-10-06 07:45:01 +0200

    -

--- web/app.php
+++ web/app.php
@@ -3,9 +3,7 @@ date_default_timezone_set('UTC');
 
 use Symfony\Component\HttpFoundation\Request;
 
-/**
- * @var Composer\Autoload\ClassLoader
- */
+/** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 
