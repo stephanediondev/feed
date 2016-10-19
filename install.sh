430f03782bff31568992fcc91f12146322da793d
Stéphane Dion <divers@sdion.net>
2016-10-19 17:45:33 +0200

    -

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
 git init
 git remote add origin https://github.com/readerself/readerself-symfony.git
+git fetch origin
+git reset --hard origin/master
 
+EXPECTED_SIGNATURE=$(wget https://composer.github.io/installer.sig -O - -q)
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php --filename=composer
-php -r "unlink('composer-setup.php');"
+ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+
+if [ "$EXPECTED_SIGNATURE" = "$ACTUAL_SIGNATURE" ]
+then
+    php composer-setup.php --filename=composer --quiet
+fi
+rm composer-setup.php
+
 
 SYMFONY_ENV=prod ./composer install --no-dev -o --prefer-dist
 bin/console doctrine:fixtures:load --append --fixtures="src/Readerself/CoreBundle/DataFixtures"
--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 git fetch origin
 git reset --hard origin/master
+
 SYMFONY_ENV=prod ./composer install --no-dev -o --prefer-dist
 cd web/client
 bower install
