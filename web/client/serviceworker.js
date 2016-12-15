07355ea884d20bc9a7e702a3e0a6d00462baaf5c
Stéphane Dion <divers@sdion.net>
2016-12-15 12:37:45 +0100

    -

--- web/client/serviceworker.js
+++ web/client/serviceworker.js
@@ -1,8 +1,49 @@
-self.addEventListener('install', function() {
-    self.skipWaiting();
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('readerself-v1').then(cache => {
+            return cache.addAll([
+                'manifest.json',
+                'index.html',
+                'app/core.css',
+                'app/core.js',
+                'app/routes.js',
+                'app/shortcuts.js',
+                'app/icons/icon-32x32.png',
+                'app/icons/icon-192x192.png',
+                'app/icons/icon-512x512.png',
+                'app/translations/en.json',
+                'app/translations/fr.json',
+                'app/views/author.html',
+                'app/views/category.html',
+                'app/views/feed.html',
+                'app/views/item.html',
+                'app/views/member.html',
+                'app/views/misc.html',
+                'vendor/dialog-polyfill/dialog-polyfill.css',
+                'vendor/material-design-lite/material.min.css',
+                'vendor/jquery/dist/jquery.min.js',
+                'vendor/dialog-polyfill/dialog-polyfill.js',
+                'vendor/material-design-lite/material.min.js',
+                'vendor/store.js/store.min.js',
+                'vendor/jquery-i18n/jquery.i18n.min.js',
+                'vendor/moment/min/moment.min.js',
+                'vendor/jquery.scrollTo/jquery.scrollTo.min.js',
+                'vendor/handlebars/handlebars.min.js'
+            ]).then(() => self.skipWaiting());
+        })
+    );
 });
 
-self.addEventListener('activate', function() {
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate',  event => {
+    event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('push', function(event) {
