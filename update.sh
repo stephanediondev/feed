b6d087da134905ac48f003a1d9a2b2d8c32e79eb
Stéphane Dion <divers@sdion.net>
2018-01-26 17:16:16 +0100

    -

--- update.sh
+++ update.sh
@@ -2,6 +2,8 @@ git fetch origin
 git reset --hard origin/master
 
 SYMFONY_ENV=prod ./composer update --no-dev -o --prefer-dist
+bin/console doctrine:schema:update
+
 cd web/client
 bower update
 cd ../../
