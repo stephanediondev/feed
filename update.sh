ee7ca55e4029236e440d12feb0d41ca379ecc11d
Stéphane Dion <divers@sdion.net>
2019-07-05 05:43:38 +0200

    improve update.sh

--- update.sh
+++ update.sh
@@ -4,4 +4,5 @@ git reset --hard origin/master
 SYMFONY_ENV=prod ./composer install --no-dev -o --prefer-dist
 bin/console doctrine:schema:update
 
+rm -rf web/client/node_modules
 yarn install --modules-folder=web/client/node_modules --production=true
