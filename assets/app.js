cb19b5df5fe44b37563fff60e474b58f1e77a816
Stéphane Dion <divers@sdion.net>
2023-04-01 08:39:18 +0200

    remove stimulus stuff

--- assets/app.js
+++ assets/app.js
@@ -8,9 +8,6 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
-// start the Stimulus application
-import './bootstrap';
-
 var bootstrap = require('bootstrap');
 
 require('jquery');
