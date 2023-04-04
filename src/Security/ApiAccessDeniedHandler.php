9e80d9e6b964e8055c67f03fd15f1dc1e988851a
Stéphane Dion <divers@sdion.net>
2023-04-04 20:03:11 +0200

    add access denied handler

---
+++ src/Security/ApiAccessDeniedHandler.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class ApiAccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    public function handle(Request $request, AccessDeniedException $accessDeniedException): JsonResponse
+    {
+        return new JsonResponse(null, JsonResponse::HTTP_FORBIDDEN);
+    }
+}
