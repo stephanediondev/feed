e6a8f218f5518ba05691af9068a78886a1d136a5
Stéphane Dion <divers@sdion.net>
2023-04-16 17:23:27 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ git reset --hard origin/main
 composer install
 bin/console doctrine:migrations:migrate -n
 
-sed -i "s/VERSION =.*/VERSION = '"$(date +%Y-%m-%d.%H-%M-%S)"';/g" public/serviceworker.js
-
 yarn install
 yarn run build
+
+sed -i "s/VERSION =.*/VERSION = '"$(date +%Y-%m-%d.%H-%M-%S)"';/g" public/serviceworker.js
