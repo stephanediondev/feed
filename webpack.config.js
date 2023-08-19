da6a59e066d03ed0b0e80681a65920d33282f4f7
Stéphane Dion <divers@sdion.net>
2023-08-19 07:30:26 +0200

    npm upgrade

--- webpack.config.js
+++ webpack.config.js
@@ -68,20 +68,6 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     .autoProvidejQuery()
-
-    .enablePostCssLoader()
-    .copyFiles({
-        from: './assets/images',
-
-        // optional target path, relative to the output dir
-        // to: 'images/[path][name].[ext]',
-
-        // if versioning is enabled, add the file hash too
-        to: 'images/[path][name].[hash:8].[ext]',
-
-        // only copy files matching this pattern
-        //pattern: /\.(png|jpg|jpeg)$/
-    })
 ;
 
 module.exports = Encore.getWebpackConfig();
